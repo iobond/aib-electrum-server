bb7b70929ceae5c6a5adb7e27d5f849c855f5277
AIB Core Development <aib@iobond.com>
2017-12-23 00:31:31 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 mkdir -p /root/TRASH
-curl -LOk https://github.com/iobond/aib-explorer-docker/releases/download/aib-1224912/aib-1224912.tgz -o /root/TRASH/aib-blockdata.tgz
+curl -Lk https://github.com/iobond/aib-explorer-docker/releases/download/aib-1224912/aib-1224912.tgz -o /root/TRASH/aib-blockdata.tgz
 mkdir -p /home/electrum.aib.one
 tar xf /root/TRASH/aib-blockdata.tgz -C /home/electrum.aib.one/
-curl -LOk https://github.com/iobond/aib-explorer-docker/releases/download/aibd-v5.13.3.13/aibd.tgz -o aibd.tgz 
+curl -Lk https://github.com/iobond/aib-explorer-docker/releases/download/aibd-v5.13.3.13/aibd.tgz -o aibd.tgz 
 tar xf aibd.tgz -C /home/electrum.aib.one/.aib/
 
 docker run  --restart=always -v /home/electrum.aib.one:/home/blockdata -v /home/.logs:/root/.logs -d -p 5037:5037 -p 31415 --name electrum.AIB.one canadianbitcoin/electrum.aib.one:v2 /root/start.sh
